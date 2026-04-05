b71e596238f66391409a86799e93135838576e2b
yusuf <atilayusuf11@gmail.com>
2026-04-05 12:01:39 +0300

    ilk python odevi

---
+++ carpma.py
@@ -0,0 +1,12 @@
+
+def carpma(a,b):
+	return a*b
+
+
+
+def main():
+	print("fenerbahçe")
+	print(carpma(4,2))
+
+if __name__ == '__main__':
+	main()
---
+++ çarpma.py
@@ -0,0 +1,12 @@
+
+def çarpma(a,b)
+	return a+b
+
+
+
+def main():
+	print("fenerbahçe")
+	print(çarpma(4,2))
+
+if __name__ == '__main__':
+	main()
